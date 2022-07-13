528f5bf746983715ba2d91729d818ec9e88f3cd2
MohamedBenArbi <88594502+MohamedBenArbi@users.noreply.github.com>
2022-07-13 21:07:30 +0100

    Created Basic React App Project added all images needed Coded basic App logic Add APi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+var dat;
+
 function App() {
+
+  function grab() {
+
+    fetch("https://api.adviceslip.com/advice").then(res => res.json()).then(ad => {
+      localStorage.setItem("adv", ad.slip.advice);
+      localStorage.setItem("id", ad.slip.id);
+
+      setstate(ad.slip.advice)
+    })
+
+
+  }
+
+  const adv = localStorage.getItem("adv");
+  const id = localStorage.getItem("id");
+
+
+  const [state, setstate] = useState()
+
+  useEffect(() => {
+    setstate(adv)
+  }, [adv])
+
+
+
+
+
   return (
     <>
-      <div></div>
+      <div className='flex items-center justify-center w-screen h-screen bg-Bl-100'>
+      </div>
     </>
   );
 }
